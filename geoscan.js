3579cdebc31232ae3ecf655a7fac172812bb4257
RampantOwl <trygve@grimnes.org>
2014-07-12 00:26:40 +0000

    Nice table output.

--- geoscan.js
+++ geoscan.js
@@ -3,78 +3,90 @@ var timers    = require ( 'timers' )
 var freegeoip = require( 'node-freegeoip' )
 var columnify = require( 'columnify')
 var dns       = require( 'dns')
+var Table =    require('cli-table');
 
 var socket    = raw.createSocket( { protocol : raw.Protocol.TCP } )
-var ip_active = {}
+var active_connections = {}
 
 console.log( '\u001B[2J\u001B[0;0f' )
 console.log("Started listening ..please wait")
+function createConnectionObject( addr,callback ){
+    var connection = {
+	dns_name  : 'unresolved',
+	source    : addr,
+	location  : {
+	    country_name : 'unresolved'
+	},
+	protocol  : 'TCP',
+	discovered  : new Date(),
+	last_heard : new Date(),
+	lost : false,
+    }
+    
+    dns.reverse(addr,function( err, addresses){
+	connection.dns_name = (err) ? addresses : "Could not resolve"
+	if ( err)
+	    connection.dns_name = "Could not resolve"
+	
+	if ( !addresses)
+	    connection.dns_name = "Resolve Error"
+	else
+	    connection.dns_name = addresses[0]
 
+	
+	
+	freegeoip.getLocation(addr , function( err , location){
+	    if( err )
+		callback(connection)
+	    
+	    connection.location = location
+	    callback(connection)
+	})
+    })
+}
 socket.on( "on" , function( buffer , addr ) {
     
-    var packet = {
-	source    : addr,
-	location  : {},
-	protocol  : 'TCP',
-	connected  : new Date(),
-	last_heard : new Date(),
-	disconnected : false,
-    }
-    
-    freegeoip.getLocation(addr , function( err , local){
-	packet.location = local
-	ip_active[packet.source] = packet
+    createConnectionObject(addr, function(connection_object) {
+	active_connections[addr] = connection_object
     })
-
+    
 })
-socket.on( "message" , function( buffer , addr ) {
-    var packet = {
-	source : addr,
-	protocol : 'TCP',
-	discovered : new Date(),
-	last_heard : new Date(),
-	location : {
-	    country_name : 'N/A'
-	}
-    }
-    if( typeof(ip_active[addr]) == 'undefined' ){
-	    var packet = {
-		source   : addr,
-		location : {},
-		protocol : 'TCP',
-		discovered : new Date(),
-		last_heard : new Date(),
-	    }
-	
-	freegeoip.getLocation(addr , function( err , local){
-	    packet.location = local
-	    ip_active[packet.source] = packet
-	})
-    }else{
-	ip_active[packet.source].last_heard= new Date();
-    }
+socket.on( "message" , function( buffer , addr ) {    
+    createConnectionObject(addr, function(connection_object) {
+	active_connections[addr] = connection_object
+    })
 })
-
 
 socket.on( "close" , function( buffer , addr ) {
-    ip_active[packet.source].disconnected = new Date();
-    delete(ip_active[addr])
+    if (!active_connections[addr])
+	return
+    
+    active_connections[addr].lost = new Date();
+    delete(active_connections[addr])
 })
 
 
 timers.setInterval(function(){
-    var connection_count = Object.keys(ip_active).length
+    var connection_keys = Object.keys(active_connections)
+    var connection_count = connection_keys.length
     
     console.log( '\u001B[2J\u001B[0;0f' )    
-    console.log ( 'CONNECTION COUNT: ' + connection_count)
 
-    Object.keys(ip_active).forEach(function( item ) {
-	packet = ip_active[item]
-	dns.lookup( packet.source, function( err , addr ){
-
-	    console.log( packet.last_heard.toString() + "\t" + packet.source + "\t"+ addr + "\t\t" + packet.location.country_name )
-	})
-	
+    var table = new Table({
+	head : ['Discovered','Country','Address','Reverse DNS'],
+	colWidths: [30, 20]
     })
+    for ( var i=0; connection_keys.length > i; i++){
+	connection = active_connections[connection_keys[i]]
+	table.push([connection.discovered,
+		    connection.location.country_name,
+		    connection.source,
+		    connection.dns_name
+		   ])
+    }
+
+    console.log(table.toString());
+    
+    
 
 },2000 )

2b4fe30542ca6e37930a89cb33448f4d8c326d04
RampantOwl <trygve@grimnes.org>
2014-07-13 11:01:16 +0000

    Added schema

---
+++ ConnectionSchema.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dns_name : String,
+    source : String,
+    country_name : String,
+    protocol : String,
+    discovered : Date,
+    last_heard : Date,
+    lost : Date,
+    bytecount : Number,
+    last_bytecount : Number
+}
